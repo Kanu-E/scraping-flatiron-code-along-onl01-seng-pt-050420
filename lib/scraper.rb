73be8f36916879569d5ded0c8332ac0a47d20af0
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-18 22:05:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,16 @@ class Scraper
   def get_courses
     self.get_page.css(".post")
   end
-  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
   
    
 end
